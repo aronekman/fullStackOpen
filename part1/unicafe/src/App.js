dd3eeef47cbd2ea7e8a9827f22576379034a2592
aronekman <aron.ekman@aalto.fi>
2022-03-19 14:38:55 +0200

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = () => good + neutral + bad;
+  const average = () => (good - bad) / all();
+  const positive = () => (good * 100) / all();
   return (
     <div>
       <h1>give feedback</h1>
@@ -15,6 +18,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all()}</p>
+      <p>average {average()}</p>
+      <p>positive {positive()} %</p>
     </div>
   );
 };
