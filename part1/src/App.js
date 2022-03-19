f285f3aeb25896976a6b1d7e020e2562d35d9265
aronekman <aron.ekman@aalto.fi>
2022-03-19 10:56:12 +0200

    1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,52 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
 
-export default App
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.number}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.name1} number={props.number1} />
+      <Part name={props.name2} number={props.number2} />
+      <Part name={props.name3} number={props.number3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.totalNumber}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        name1={part1}
+        number1={exercises1}
+        name2={part2}
+        number2={exercises2}
+        name3={part3}
+        number3={exercises3}
+      />
+      <Total totalNumber={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
