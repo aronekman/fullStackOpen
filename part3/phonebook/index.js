f4ae7efea8f8483243bb71c8d742609592bf2b80
aronekman <aron.ekman@aalto.fi>
2022-04-01 22:45:51 +0300

    3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -49,6 +49,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
