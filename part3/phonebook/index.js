f98c5d8375e55a9e8f3afa87885f135d6ef36862
aronekman <aron.ekman@aalto.fi>
2022-04-02 20:29:12 +0300

    3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -88,7 +88,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
