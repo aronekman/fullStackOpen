cb22759c06623cc8b3b1b5f8702acc71c5195063
aronekman <aron.ekman@aalto.fi>
2022-04-04 06:26:35 +0300

    3.13

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -19,29 +19,6 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => response.json(persons));
 });
