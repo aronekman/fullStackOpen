4946f3e7618b2a4ab9268413bca36b2fb55bf6c6
aronekman <aron.ekman@aalto.fi>
2022-04-04 06:32:28 +0300

    3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -49,23 +49,19 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
-  console.log(body);
   if (!body.name) {
     return response.status(400).json({ error: 'name missing' });
   }
   if (!body.number) {
     return response.status(400).json({ error: 'number missing' });
   }
-  if (persons.some((p) => p.name === body.name)) {
-    return response.status(400).json({ error: 'name must be unique' });
-  }
-  const person = {
-    id: Math.floor(Math.random() * 100000),
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  });
+
+  person.save().then((savedPerson) => response.json(savedPerson));
 });
 
 const PORT = process.env.PORT;
