b0a48a6c65d4ab1bf3368ad6cc5b7d61d596e734
aronekman <aron.ekman@aalto.fi>
2022-04-10 15:42:14 +0300

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = blogs => {
   return blogs.reduce((previous, current) => previous + current.likes, 0);
 };
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return {};
+  const favorite = blogs.reduce((previous, current) =>
+    previous.likes > current.likes ? previous : current
+  );
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
