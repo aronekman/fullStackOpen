2d50c6b9b43edf42b9bf5304e26bdd89e283b32b
aronekman <aron.ekman@aalto.fi>
2022-04-12 21:39:21 +0300

    4.15

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 const initialBlogs = [
   {
@@ -48,4 +49,9 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON());
 };
 
-module.exports = { initialBlogs, blogsInDb };
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map(u => u.toJSON());
+};
+
+module.exports = { initialBlogs, blogsInDb, usersInDb };
