4dbca77c7b24474e693dda56d6f252ae79e5980e
aronekman <aron.ekman@aalto.fi>
2022-04-18 11:10:53 +0300

    5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -17,6 +17,14 @@ const App = () => {
     getInitialBlogs();
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async event => {
     event.preventDefault();
 
@@ -24,7 +32,7 @@ const App = () => {
       username,
       password,
     });
-    console.log(user);
+    window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
     setUser(user);
     setUsername('');
     setPassword('');
@@ -54,10 +62,18 @@ const App = () => {
     </form>
   );
 
+  const handleLogout = () => {
+    window.localStorage.clear();
+    setUser(null);
+  };
+
   const blogsForm = () => (
     <>
       <h2>blogs</h2>
-      <p>{user.name} loggin in</p>
+      <p>
+        {user.name} loggin in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
       ))}
