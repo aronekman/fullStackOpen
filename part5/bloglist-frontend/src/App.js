635fff35ecf97cda8a39b04a741b6c3a924fadbb
aronekman <aron.ekman@aalto.fi>
2022-05-04 23:45:51 +0300

    5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -114,9 +114,11 @@ const App = () => {
             <h2>create new</h2>
             <BlogForm createBlog={createBlog} />
           </Togglable>
-          {blogs.map(blog => (
-            <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog => (
+              <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+            ))}
         </div>
       )}
     </div>
