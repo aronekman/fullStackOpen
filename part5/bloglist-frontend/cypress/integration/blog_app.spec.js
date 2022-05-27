ce7a55cad8fac28c04531f472a6a1dc903c4960b
aronekman <aron.ekman@aalto.fi>
2022-05-27 18:57:24 +0300

    5.22

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -7,6 +7,7 @@ describe('Blog app', function () {
       password: 'root',
     };
     cy.request('POST', 'http://localhost:3003/api/users/', user);
+
     cy.visit('http://localhost:3000');
   });
 
@@ -56,6 +57,71 @@ describe('Blog app', function () {
       cy.contains('view').click();
       cy.contains('testUrl.com');
       cy.contains('likes 0');
+    });
+    describe('and some blogs exist', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'title1',
+          author: 'author1',
+          url: 'url1.com',
+          likes: 0,
+        });
+        cy.createBlog({
+          title: 'title2',
+          author: 'author2',
+          url: 'url2.com',
+          likes: 2,
+        });
+        cy.createBlog({
+          title: 'title3',
+          author: 'author3',
+          url: 'url3.com',
+          likes: 1,
+        });
+      });
+      it('one of those can be liked', function () {
+        cy.contains('title1').parent().as('theBlog');
+
+        cy.get('@theBlog').contains('view').click();
+        cy.get('@theBlog').contains('like').click();
+        cy.get('@theBlog').should('contain', 'likes 1');
+      });
+      it('one of those can be deleted', function () {
+        cy.contains('title2').parent().as('theBlog');
+
+        cy.get('@theBlog').contains('view').click();
+        cy.get('@theBlog').contains('remove').click();
+        cy.get('.blog').should($blog => {
+          expect($blog).to.have.length(2);
+        });
+      });
+      it('other users can not remove blogs', function () {
+        const user2 = {
+          name: 'other user',
+          username: 'user2',
+          password: 'pass2',
+        };
+        cy.request('POST', 'http://localhost:3003/api/users/', user2);
+
+        cy.contains('logout').click();
+        cy.login({ username: 'user2', password: 'pass2' });
+        cy.wait(500);
+        cy.get('button:contains(view)').click({ multiple: true });
+        cy.get('button').contains('remove').should('not.exist');
+      });
+      it('they are ordered according to likes ', function () {
+        cy.get('.blog').eq(0).should('contain', 'title2');
+        cy.get('.blog').eq(1).should('contain', 'title3');
+        cy.get('.blog').eq(2).should('contain', 'title1');
+        cy.get('button:contains(view)').click({ multiple: true });
+        cy.contains('title1').parent().contains('like').click();
+        cy.wait(500);
+        cy.contains('title1').parent().contains('like').click();
+        cy.wait(500);
+        cy.contains('title1').parent().contains('like').click();
+        cy.wait(500);
+        cy.get('.blog').eq(0).should('contain', 'title1');
+      });
     });
   });
 });
