188c5f2134c482a4b4404ebe8c08c346cfdae069
aronekman <aron99ekman@gmail.com>
2022-09-27 15:40:21 +0300

    8.3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -86,10 +86,16 @@ const typeDefs = gql`
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -97,7 +103,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: root => {
+      return books.filter(book => book.author === root.name).length;
+    }
   }
 };
 
