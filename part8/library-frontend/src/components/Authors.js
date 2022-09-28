a2ff04fe37eda0c038b95ac0a6404f909159de7d
aronekman <aron99ekman@gmail.com>
2022-09-28 19:59:41 +0300

    8.8

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from '../queries';
+
+const Authors = props => {
+  const result = useQuery(ALL_AUTHORS);
+  if (!props.show) {
+    return null;
+  }
+  if (result.loading) return null;
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {result.data.allAuthors.map(a => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
