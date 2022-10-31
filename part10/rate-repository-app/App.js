1fc341abd71b02421ea827a39e4a107505048818
aronekman <aron99ekman@gmail.com>
2022-10-31 22:40:37 +0200

    10.3

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
+const App = () => {
+  return <Main />;
+};
 
-export default function App() {
-  console.log('moro');
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+export default App;
